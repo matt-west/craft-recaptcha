bb0df861d3d846c9c839b13364cb1f8296610140
David Kurz <dk@nevoxo.com>
2020-11-13 16:04:07 +0100

    Added german translation

---
+++ src/translations/de/recaptcha.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Craft reCAPTCHA plugin for Craft CMS 3.x
+ *
+ * Integrate Google’s reCAPTCHA into your forms.
+ *
+ * @link      https://mattwest.io
+ * @copyright Copyright (c) 2018 Matt West
+ */
+
+/**
+ * Craft reCAPTCHA German Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('craft-recaptcha', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    David Kurz (nevoxo.com)
+ * @package   CraftRecaptcha
+ * @since     1.0.0
+ */
+return [
+    'Craft reCAPTCHA plugin loaded' => 'Craft reCAPTCHA Plugin wurde geladen',
+    'Site Key' => 'Website Key',
+    'Secret Key' => 'Secret Key',
+    'Enter your reCAPTCHA site key.' => 'Gib deinen reCAPTCHA Website Key ein.',
+    'Enter your reCAPTCHA secret key.' => 'Gib deinen reCAPTCHA Secret Key ein',
+    'Validate contact forms?' => 'Kontaktformulare validieren?',
+    'Enable to automatically validate reCAPTCHAs when using the official [Craft CMS contact form plugin](https://github.com/craftcms/contact-form).' => 'Aktivieren um Formulare automatisch zu validieren, wenn das offizielle [Craft CMS Contact Form Plugin](https://github.com/craftcms/contact-form) verwendet wird.',
+    'Please verify you are human.' => 'Bitte das Captcha lösen um zu bestätigen, dass die Anfrage nicht automatisch versendet wurde.'
+];
