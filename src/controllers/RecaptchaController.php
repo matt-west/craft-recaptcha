afbb46211402ad6a3a3270c0b6c0a876ac6dcce0
danbrellis <danbrellis@gmail.com>
2022-03-09 16:17:25 -0500

    Adds v3 captcha support to recaptcha/verify-submission controller

--- src/controllers/RecaptchaController.php
+++ src/controllers/RecaptchaController.php
@@ -26,17 +26,29 @@ class RecaptchaController extends Controller
 		// grab the intended action (required)
 		$action = $request->getRequiredParam('verified-action');
 		
-		// grab the recaptcha response (required)
-		$captcha = $request->getRequiredParam('g-recaptcha-response');
+		// grab the reCaptcha action
+		$reCaptchaAction = $request->getParam('recaptcha-action');
+
+		// grab the reCaptcha score threshold
+		$threshold = $request->getParam('recaptcha-threshold');
+
+		//Verify reCaptcha fields, first trying V3
+		$version = 3;
+		$captcha = Craft::$app->getRequest()->getParam('recaptcha_response');
+		if ($captcha === null) {
+			//no V3 param exists, try V2...
+			$version = 2;
+			$captcha = Craft::$app->getRequest()->getRequiredParam('g-recaptcha-response');
+		}
 		
 		// run these past the verify() function
-		$verified = CraftRecaptcha::$plugin->craftRecaptchaService->verify($captcha);
+		$verified = CraftRecaptcha::$plugin->craftRecaptchaService->verify($captcha, $version, $reCaptchaAction, $threshold);
 		
 		// if it's verified, then pass it on to the intended action, otherwise set a session error and return null
 		if ($verified) {
 			return Controller::run('/' . $action, func_get_args()); // run the intended action (add / to force it's scope to be outside the plugin) with all the params passed to this controller action
 		} else {
-            Craft::$app->getSession()->setError(Craft::t('site', 'Unable to verify your submission.'));
+			Craft::$app->getSession()->setError(Craft::t('site', 'Unable to verify your submission.'));
 			return null;
 		}
 	}
