87d75d5bc3e0fe5af709dc1f5238932cc0a93104
honsa <straka@8038.ch>
2022-05-21 20:18:47 +0200

    Define type

--- src/CraftRecaptcha.php
+++ src/CraftRecaptcha.php
@@ -65,7 +65,7 @@ class CraftRecaptcha extends Plugin
      *
      * @var string
      */
-    public $schemaVersion = '1.0.0';
+    public string $schemaVersion = '1.0.0';
 
     // Public Methods
     // =========================================================================
